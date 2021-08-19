977b480fd3454efdd5f02a4284fbc9cae3d5952c
Alex Knight <aknight@atlassian.com>
2021-08-19 15:59:10 +1000

    attempt to publish to PAC

--- .circleci/publish-nucleus-docker.sh
+++ .circleci/publish-nucleus-docker.sh
@@ -12,11 +12,12 @@
 # First 8 characters of SHA
 export COMMIT=${CIRCLE_SHA1:0:8}
 # Docker Hub Repo
-export REPO=atlassian/nucleus
+export PAC=docker-public.packages.atlassian.com
+export REPO=$PAC/atlassian/nucleus
 # Target tag, latest on master, branch otherwise
 export TAG=`if [ "$CIRCLE_BRANCH" == "master" ]; then echo "latest"; else echo $CIRCLE_BRANCH ; fi`
 
-docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+docker login -u $PAC_DOCKER_USER -p $PAC_DOCKER_PASSWORD $PAC
 docker build -f Dockerfile -t $REPO:$COMMIT .
 docker tag $REPO:$COMMIT $REPO:$TAG
 docker tag $REPO:$COMMIT $REPO:circle-$CIRCLE_BUILD_NUM
